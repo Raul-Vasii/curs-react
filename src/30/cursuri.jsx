b37cf5bc43cf347e0af08db48764dc56d9599575
Vasii Raul <vasiiraul01@gmail.com>
2026-01-31 12:31:45 +0200

    sesiunea 30

---
+++ src/30/cursuri.jsx
@@ -0,0 +1,35 @@
+export function Cursuri() {
+  const cursuri = [
+    { id: 101, titlu: "Introducere în React", categorie: "Programare" },
+
+    { id: 102, titlu: "Design UI/UX", categorie: "Design" },
+
+    { id: 103, titlu: "Baze de Date SQL", categorie: "Programare" },
+  ];
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Cursurile Mele</h1>
+
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {/* PASUL 2 & 3: Folosește .map() aici pentru a genera lista */}
+
+        {cursuri.map((curs) => {
+          return (
+            <li key={curs.id} className="curs-item">
+              {curs.titlu}
+            </li>
+          );
+        })}
+
+        {/* Exemplu de structură dorită pentru fiecare curs:
+
+            <li className="curs-item"> 📚 Titlu Curs </li> 
+
+        */}
+      </ul>
+
+      {/* PASUL 4: Nu uita de atributul 'key' pe <li>! */}
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,32 @@ import { State } from "./29/state";
 import { ToggleButton } from "./29-tema/E1-butonDeToggle";
 import { LiveInput } from "./29-tema/E2-InputControlled";
 import { CounterCuLimita } from "./29-tema/E3-CounterCuLimită";
+import { Todos } from "./30/todos";
+import { Cursuri } from "./30/cursuri";
 
 function App() {
   // console.log(addTwo(1, 2), "rezultat adunare");
 
   return (
-
+    // SESIUNEA 30
     <>
-    <HtmlFor />
-    <Click />
-    <Input />
-    <State />
-    <ToggleButton />
-    <LiveInput />
-    <CounterCuLimita />
+     <Todos />
+     <Cursuri/>
+
     </>
+
+    // SESIUNEA 29
     // <>
+    // <HtmlFor />
+    // <Click />
+    // <Input />
+    // <State />
+    // <ToggleButton />
+    // <LiveInput />
+    // <CounterCuLimita />
+    // </>
+    // <>
+    // SESIUNEA 28
     //   <input id="" className="" />
 
     //   <Header userName='Vasi' />
